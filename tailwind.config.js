4727d74292ef5694dc77f1045cf28389afc5d85d
yamori-masato <yamorimasato@gmail.com>
2021-10-30 17:44:29 +0900

    update: colorを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   purge: ["./src/**/*.{ts,tsx}"],
   darkMode: false,
   theme: {
+    colors: {
+      black: "#000",
+      white: "#fff",
+      current: "currentColor",
+      transparent: "transparent",
+
+      gray: "#323237", // rgb(50, 50, 55)
+      grayDark: "#0f0f11", // rgb(15, 15, 17)
+    },
     extend: {},
   },
   variants: {
