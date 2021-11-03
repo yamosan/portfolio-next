0c6f275f33d7525799968aa7a1898b156f606edf
yamori-masato <yamorimasato@gmail.com>
2021-11-03 19:30:28 +0900

    update: ssrを無効化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/shared/styles/global.css";
 
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 
 import Layout from "@/shared/components/layouts";
 import BgStateProvider from "@/shared/store/bgState";
@@ -16,4 +17,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-export default App;
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
