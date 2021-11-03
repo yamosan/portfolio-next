04b238bf8bbc2ccbdc77c6dda548a554ee324555
yamori-masato <yamorimasato@gmail.com>
2021-11-03 21:09:59 +0900

    framer-motionでページトランジションを実装

--- src/modules/contact/index.tsx
+++ src/modules/contact/index.tsx
@@ -5,7 +5,7 @@ import { Underlined } from "@/shared/components/basics/Underlined";
 export const Contact: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-full">
+      <div className="flex flex-col items-center justify-center h-full min-h-screen">
         <Underlined as="h2">CONTACT</Underlined>
         <div className="pt-10 text-white text-center">
           <p className="">ご連絡は下記gmailまでお願いします</p>
--- src/shared/components/layouts/index.tsx
+++ src/shared/components/layouts/index.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 import type { FC } from "react";
 
 import { Header } from "./Header";
@@ -6,18 +8,27 @@ import { Header } from "./Header";
 const Background = dynamic(() => import("./background"), { ssr: false });
 
 const Layout: FC = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
       <Background />
       <div
         id="root"
+        className="relative z-10"
         // TODO: スクロールバーの色を変える。ヘッダーと被らないようにする
         // className="relative h-screen scrollbar-thin scrollbar-track-gray scrollbar-thumb-main scrollbar-thumb-rounded-full"
-        className="relative h-screen"
       >
         <Header />
-
-        {children}
+        <motion.div
+          key={router.pathname}
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          {children}
+        </motion.div>
       </div>
     </>
   );
